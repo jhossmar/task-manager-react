a85e1c0d0ef0827aa0fa6d149d1e91b50b1fb87c
jhossmar <jhosmar_ramod@hotmail.com>
2026-09-12 07:21:05 -0400

    Add code coverage

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,16 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.js'
+    setupFiles: './src/test/setup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 20,
+        functions: 20,
+        branches: 20,
+        statements: 20,
+      },
+    },
   },
 })
